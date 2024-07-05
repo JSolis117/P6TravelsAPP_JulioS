7b66398e3d27969420101d94fde55cce7bc0ab6a
julio <julio@JULIO>
2024-07-04 21:03:07 -0600

    Add project files.

---
+++ P6TravelsAPP_JulioS/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using P6TravelsAPP_JulioS.Views;
+
+
+
+namespace P6TravelsAPP_JulioS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+    }
+}
---
+++ P6TravelsAPP_JulioS/Views/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace P6TravelsAPP_JulioS.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
